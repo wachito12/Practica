0e3d9b6844a8ed57960331e0121e05fbb2f3a856
wachito12 <wachitoherrera698@gmail.com>
2023-07-03 08:04:06 -0500

    se sube el proyecto

--- testUceAPI/uce/ai/openaitest.py
+++ testUceAPI/uce/ai/openaitest.py
@@ -8,28 +8,18 @@ class Document(BaseModel):
 
 def inference(prompt: str) -> list:
     openai.organization = 'org-FlF7DjfN8kf6smfJafiv4p82'
-    openai.api_key = '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'
+    openai.api_key = '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'
     print('[PROCESANDO]'.center(40, '-'))
-
-    # Diccionario para mapear letras a números
-    letter_to_number = {
-        'a': '1',
-        'b': '2',
-        'c': '3',
-        'd': '4',
-        'e': '5',
-        # Agrega el resto de las letras y números según sea necesario
-    }
-
-    # Reem-plaza las letras por numeral en el prompt
-    prompt_numbers = ''.join(letter_to_number.get(char.lower(), char) for char in prompt)
 
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "Escribe alreves la palabra hola "},
-            {"role": "user", "content": prompt_numbers},
-            {"role": "assistant", "content": ""}
+            {"role": "system", "content": "Calculame el factorial 10, cada numero ingresado, calcula el factorial,"
+                                          "y si es un texto que me presente error"
+                                          ""
+                                          ""},
+            {"role": "user", "content": prompt},
+
 
         ],
         temperature=0
